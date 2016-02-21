1766626f8b24a43f7d98da351fa01ca63093c477
Dagothig <guillaumenoelmartel@gmail.com>
2016-02-21 13:37:33 -0500

    Stagecoach / wells fargo eternal draw fix

--- server/game/cards/drawing.js
+++ server/game/cards/drawing.js
@@ -1,23 +1,24 @@
 var Card = aReq('server/game/cards/card'),
     misc = aReq('server/misc');
 
-function handleDrawCard(step, card, amount) {
+function handleDrawCard(step, card, amount, onResolved) {
     step.player.hand.discard(card.id);
     step.player.hand.drawFromPile(amount);
+    onResolved();
 }
 
 function Stagecoach(suit, rank) {
     Card.call(this, 'stagecoach', suit, rank, Card.types.brown);
 }
 misc.extend(Card, Stagecoach, {
-    handlePlay: function(step, onResolved) { handleDrawCard(step, this, 2); }
+    handlePlay: function(step, onResolved) { handleDrawCard(step, this, 2, onResolved); }
 });
 
 function WellsFargo(suit, rank) {
     Card.call(this, 'wellsfargo', suit, rank, Card.types.brown);
 }
 misc.extend(Card, WellsFargo, {
-    handlePlay: function(step, onResolved) { handleDrawCard(step, this, 3); }
+    handlePlay: function(step, onResolved) { handleDrawCard(step, this, 3, onResolved); }
 });
 
 module.exports = {
